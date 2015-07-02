6f428d8c30a406f3bebcb9fd97bc6d2775c74573
Móréh Tamás <morta@digitus.itk.ppke.hu>
2015-07-02 10:41:50 +0200

    Several CorpusReaders added.

---
+++ corpusreader/corpus_reader.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env Python3
+###############################################################################
+# Copyright (c) 2015 Móréh, Tamás
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Lesser Public License v3
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/
+#
+# This file is part of PurePos-Python3.
+#
+# PurePos-Python3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PurePos-Python3 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# Contributors:
+#     Móréh, Tamás - initial API and implementation
+##############################################################################
+
+__author__ = 'morta@digitus.itk.ppke.hu'
+
+import os
+from corpusreader.tokenreaders import BaseReader
+from corpusreader.tokenreaders import SentenceReader
+from docmodel.containers import Sentence, Paragraph, Document
+
+
+class CorpusReader(BaseReader):
+    def __init__(self, token_reader: BaseReader, linesep: str=os.linesep):
+        self.token_reader = token_reader
+        self.linesep = linesep  # todo: kell? van default. os.linesep vs "\n"
+        self.sentence_parser = SentenceReader(self.token_reader)
+        # todo: ha nem kell máshol a token_reader, akkor ne legyen attribute.
+        # A sentence_parser jó, ha attribute, vagy legyen read szintű? Hasonlóan a
+        # hunpos_corpusr_readerben
+
+    def read(self, text: str):
+        sentences = list()
+        for line in text.split(self.linesep):
+            if len(line) > 0:
+                sentences.append(self.sentence_parser.read(line))
+        paragraph = Paragraph(sentences)
+        document = Document()
+        document.append(paragraph)
+        return document
+    # todo: Na de hé! Ez egy bekezdésbe zúzza az összes mondatot!
+
---
+++ corpusreader/hunpos_corpus_reader.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env Python3
+###############################################################################
+# Copyright (c) 2015 Móréh, Tamás
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Lesser Public License v3
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/
+#
+# This file is part of PurePos-Python3.
+#
+# PurePos-Python3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PurePos-Python3 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# Contributors:
+#     Móréh, Tamás - initial API and implementation
+##############################################################################
+
+__author__ = 'morta@digitus.itk.ppke.hu'
+
+import os
+from corpusreader.tokenreaders import BaseReader
+from corpusreader.tokenreaders import SentenceReader
+from corpusreader.tokenreaders import TaggedTokenReader
+from docmodel.containers import Sentence, Paragraph, Document
+
+
+class HunPosCorpusReader(BaseReader):
+    def __init__(self):
+        self.encoding = "ISO-8859-2"
+        self.word_parser = TaggedTokenReader("\t")
+        self.sentence_parser = SentenceReader(self.word_parser, self.linesep)
+
+    def read(self, text: str):
+        sentences = list()
+        for sent in text.split(self.linesep + self.linesep):
+            if len(sent)-1 > 0:
+                sentences.append(self.sentence_parser.read(sent))
+        paragraph = Paragraph(sentences)
+        document = Document()
+        document.append(paragraph)
+        return document
+
