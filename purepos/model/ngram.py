25579f6cd865478125181738d56441b58f2da005
Móréh Tamás <morta@digitus.itk.ppke.hu>
2015-07-12 22:24:23 +0200

    NGram class implemented

---
+++ purepos/model/ngram.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env Python3
+# todo nincs kész
+###############################################################################
+# Copyright (c) 2015 Móréh, Tamás
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Lesser Public License v3
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/
+#
+# This file is part of PurePos-Python3.
+#
+# PurePos-Python3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PurePos-Python3 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# Contributors:
+#     Móréh, Tamás - initial API and implementation
+##############################################################################
+
+__author__ = 'morta@digitus.itk.ppke.hu'
+
+
+class NGram:
+    def __init__(self, tokens: list, compare_length: int=-1, new_element=None):
+        self.token_list = tokens
+        self.compare_length = compare_length
+        if new_element is not None:
+            l = list(tokens)
+            l.append(new_element)
+            self.token_list = l
+
+    def add(self, element):
+        return NGram(self.token_list, self.compare_length, element)
+
+    def __str__(self):
+        return str(self.token_list)
+
+    # todo eq hash_code, compareTo, toList -> token_list
+
+    def last(self):
+        return self.token_list[-1]
