b0b7c7d8e131cfdf99f40d9f85f0847345b03f4d
Móréh Tamás <morta@digitus.itk.ppke.hu>
2015-07-14 12:03:25 +0200

    History class added

---
+++ purepos/model/history.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env Python3
+###############################################################################
+# Copyright (c) 2015 Móréh, Tamás
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Lesser Public License v3
+# which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/
+#
+# This file is part of PurePos-Python3.
+#
+# PurePos-Python3 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PurePos-Python3 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# Contributors:
+#     Móréh, Tamás - initial API and implementation
+##############################################################################
+
+__author__ = 'morta@digitus.itk.ppke.hu'
+
+from purepos.model.ngram import NGram
+
+
+class History:
+    def __init__(self, tag_seq: NGram, log_prob: float):
+        self.tag_seq = tag_seq
+        self.log_prob = log_prob
+
+    def __eq__(self, other):
+        return isinstance(other, History) and\
+            self.tag_seq.token_list == other.tag_seq.token_list
